a901e2843cf2e67111e83d7c64fd42ab5274827c
Victor Puentes <vmpuentes@gmail.com>
2017-07-10 14:56:26 -0700

    add classes and ids and update js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-  var questions = parseInt(submit("Do you like to design webpages?"));
+  $(".result").hide();
 
-  if (questions === 1) {
+  // var questions = parseInt(submit("Do you like to design webpages?"));
+  var q1Answer = parseInt($("#q1").val());
+
+console.log(q1Answer);
+
+  if (q1Answer === 1) {
     $('#csharp').show();
-}  else {
-  $('#ruby').show();
+  } else {
+    $('#ruby').show();
   }
 });
+
+// THINGS TO CONSIDER:
+// Are we triggering an event? Y/N?
+// What is the event we should be triggering?
+// How can we collect AND use the answers to all of the questions, but only after some kind of event?
