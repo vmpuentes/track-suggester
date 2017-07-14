4f3137df34286952eea3147a1b7fdd26d45cfaea
Victor Puentes <vmpuentes@gmail.com>
2017-07-14 16:27:38 -0700

    add alert to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,30 @@
-$(document).ready(function() {
-  $(".result").hide();
-    $("form").submit(function(event) {
-      console.log("form submitted.");
+$(function() {
+  $("#formOne").submit(function(event) {
 
-      var q1Answer = parseInt($("#q1").val());
-      var q2Answer = parseInt($("#q2").val());
-      var q3Answer = parseInt($("#q3").val());
-      var q4Answer = parseInt($("#q4").val());
-      var q5Answer = parseInt($("#q5").val());
+      var q1Answer = $("input#q1").val();
+      var q2Answer = $("input#q2").val();
+      var q3Answer = $("input#q3").val();
+      var q4Answer = $("input#q4").val();
+      var q5Answer = $("input#q5").val();
 
-      $("select#q1").select(q1Answer);
-      $("select#q2").select(q2Answer);
-      $("select#q3").select(q3Answer);
-      $("select#q4").select(q4Answer);
-      $("select#q5").select(q5Answer);
+      for (index = i, )
 
-      if (q1Answer = 1) {
-        return (#csharp).show();
-        if (q2Answer )
-      }
+      if (q1Answer === "yes") {
 
+      alert("The C# programming language is most commonly used with the .NET framework. Students who are well-versed in C# programming can use it to create client applications, database applications, distributed components, and much more.Students who learn C# programming and combine it with their knowledge of the .NET framework can target larger enterprises, government agencies, and companies serving enterprise and government clients for employment.Companies using C# and .NET who have hired Epicodus graduates include Windsor and Incomm.")
+    }
+  });
+});
+  // if (q1Answer === "yes") {
+  //   return $("#results#csharp").show();
+  // } else $("#php").show();
+  // console.log("hello");
 
-
-
-  }
- $(".result").hide();
-
+  //$("form").submit(function(event) {
+  //     console.log("form submitted.");
   // var questions = parseInt(submit("Do you like to design webpages?"));
-  var q1Answer = parseInt($("#q1").val());
-    event.preventDefault();
+  // var q1Answer = parseInt($("#q1").val());
+  //   event.preventDefault();
 // console.log(q1Answer);
 
 //   if (q1Answer === 1) {
