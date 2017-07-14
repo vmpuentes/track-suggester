777e9fa7891d4406e6232b5bd6a4ca06302347d7
Victor Puentes <vmpuentes@gmail.com>
2017-07-14 12:25:15 -0700

    add Python description to index.html and add functionality to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-  event.preventDefault();
-// $(".result").hide();
+  $(".result").hide();
+    $("form").submit(function(event) {
+      console.log("form submitted.");
+
+      var q1Answer = parseInt($("#q1").val());
+      var q2Answer = parseInt($("#q2").val());
+      var q3Answer = parseInt($("#q3").val());
+      var q4Answer = parseInt($("#q4").val());
+      var q5Answer = parseInt($("#q5").val());
+
+      $("select#q1").select(q1Answer);
+      $("select#q2").select(q2Answer);
+      $("select#q3").select(q3Answer);
+      $("select#q4").select(q4Answer);
+      $("select#q5").select(q5Answer);
+
+      if (q1Answer = 1) {
+        return (#csharp).show();
+        if (q2Answer )
+      }
+
+
+
+
+  }
+ $(".result").hide();
 
   // var questions = parseInt(submit("Do you like to design webpages?"));
   var q1Answer = parseInt($("#q1").val());
-
+    event.preventDefault();
 // console.log(q1Answer);
 
 //   if (q1Answer === 1) {
